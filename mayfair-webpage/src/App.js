134ded08231625e4b5d555075be4ccd448f8266e
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-04 12:16:12 +0100

    added route for landing page

--- mayfair-webpage/src/App.js
+++ mayfair-webpage/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      Hello!
-    </div>
+    <Router>
+      <Route exact path={'/'} component={LandingPage} ></Route>
+    </Router>
   );
 }
 
