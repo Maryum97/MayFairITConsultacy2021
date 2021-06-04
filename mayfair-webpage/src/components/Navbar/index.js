97b89d7892a8e748063c5b0961a238a64a28414a
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-04 12:56:37 +0100

    edited navbar

--- mayfair-webpage/src/components/Navbar/index.js
+++ mayfair-webpage/src/components/Navbar/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Navbar() {
     return (
         <div>
-            <nav class="navbar navbar-dark bg-dark">
+            <nav class="navbar sticky-top navbar-dark bg-dark">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="/"><h1>Mayfair IT Consultancy</h1></a>
                 </div>
