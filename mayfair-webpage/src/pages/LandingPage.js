e4740416f11150943ddea334c8bf250e73e404ee
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-04 12:56:53 +0100

    added header

--- mayfair-webpage/src/pages/LandingPage.js
+++ mayfair-webpage/src/pages/LandingPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from '../components/Header';
 import Navbar from '../components/Navbar';
 
 function LandingPage() {
     return (
         <div>
             <Navbar />
+            <Header />
         </div>
     )
 }
