84a3136686e6c4d451c2fb76a038357e89ef5dd9
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-04 14:39:13 +0100

    added navbar to page

--- mayfair-webpage/src/components/Navbar/index.js
+++ mayfair-webpage/src/components/Navbar/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 function Navbar() {
-    return(
+    return (
         <div>
-
+            <nav class="navbar sticky-top navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="/"><h1>Mayfair IT Consultancy</h1></a>
+                </div>
+            </nav>
         </div>
     )
 }
--- mayfair-webpage/src/pages/LandingPage.js
+++ mayfair-webpage/src/pages/LandingPage.js
@@ -1,13 +1,12 @@
 import React from 'react';
 // import Header from '../components/Header';
-// import Navbar from '../components/Navbar';
+import Navbar from '../components/Navbar';
 
 function LandingPage() {
     return (
         <div>
-            {/* <Navbar />
-            <Header /> */}
-            Hello!
+            <Navbar />
+            {/* <Header /> */}
         </div>
     )
 }
