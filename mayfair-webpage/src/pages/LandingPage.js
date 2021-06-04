90dc2b27d9c5000af4293ee7a1789bfe32431df4
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-04 14:57:11 +0100

    added cards to page

--- mayfair-webpage/src/pages/LandingPage.js
+++ mayfair-webpage/src/pages/LandingPage.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import Carousel from '../components/Carousel';
+import InfoCard from '../components/InfoCard';
 import Navbar from '../components/Navbar';
 
 function LandingPage() {
     return (
         <div>
             <Navbar />
-            <Carousel />
+            <Container>
+                <Row>
+                    <Col xs="12">
+                        <Carousel />
+                    </Col>
+                    <Col xs="6">
+                        <InfoCard />
+                    </Col>
+                    <Col xs="6">
+                        <InfoCard />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
