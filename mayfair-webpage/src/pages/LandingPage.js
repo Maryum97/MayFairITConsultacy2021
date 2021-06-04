123d712c7082bb7673b52ab10a2114a59775bb18
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-06-04 14:36:03 +0100

    added basic layout

---
+++ mayfair-webpage/src/pages/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import Header from '../components/Header';
+// import Navbar from '../components/Navbar';
+
+function LandingPage() {
+    return (
+        <div>
+            {/* <Navbar />
+            <Header /> */}
+            Hello!
+        </div>
+    )
+}
+
+export default LandingPage;
